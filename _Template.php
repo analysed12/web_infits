44d93e90425265de91c658800664f7cf855b14e6
chetramaaseri <90443950+chetramaaseri@users.noreply.github.com>
2023-05-10 22:02:18 +0530

    template added

---
+++ _Template.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page TITLE HERE</title>
+    <?php require('constant/head.php'); ?>
+    <!-- CSS FILES GOES HERE -->
+    <!-- WE WILL NOT USE ANY KIND OF CDN -->
+<style>
+    /* CSS of Page Goes Here */
+</style>
+</head>
+
+<body>
+    <!-- Code Related to This Page Only -->
+    <!-- Script FILES GOES HERE-->
+    <?php require('constant/head.php'); ?>
+    <!-- Other Script Files -->
+<script>
+    // JS of This page Goes Here
+</script>
+</body>
+</html>
