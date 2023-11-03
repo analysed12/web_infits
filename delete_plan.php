975feb7b99e9c9b064d6f96add1372c9c900a4bd
analysed12 <84315274+analysed12@users.noreply.github.com>
2023-11-03 11:26:09 +0530

    Update delete_plan.php

--- delete_plan.php
+++ delete_plan.php
@@ -3,13 +3,14 @@ include "constant/config.php";
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    //table name is clients not subscribed clients
-    $query1 = "DELETE FROM `client` WHERE `client`.`plan`='$id'";
-    $query2 = "DELETE FROM `addclient` WHERE `addclient`.`plan_id`='$id'";
-    $sql = "DELETE FROM `create_plan`WHERE `create_plan`.`plan_id`='$id'";
+   // $query1 = "DELETE FROM `subscribedclient` WHERE `subscribedclient`.`plan`='$id'";
+   // $query2 = "DELETE FROM `addclient` WHERE `addclient`.`plan_id`='$id'";
+   
     // "DELETE FROM create_plan WHERE  = 1"?
-    $res1 = mysqli_query($conn, $query1);
-    $res2 = mysqli_query($conn, $query2);
+    //$res1 = mysqli_query($conn, $query1);
+    //$res2 = mysqli_query($conn, $query2);
+
+    $sql = "DELETE FROM `create_plan`WHERE `plan_id`='$id'";
     $result = mysqli_query($conn, $sql);
 
     if ($result) {
