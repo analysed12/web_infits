72c0cc1dc827123a2e23be35b15168f4fef1ce4d
analysed12 <84315274+analysed12@users.noreply.github.com>
2024-02-29 20:58:09 +0530

    updating the new details of the database

--- constant.php
+++ constant.php
@@ -1,7 +1,5 @@
 <?php
 
-$DEFAULT_PATH = 'http://localhost/web_infits-main2/';
-$PROFILE_IMAGE = $DEFAULT_PATH.'assets/uploads/profile/images/';
-$RECIPE_IMAGE = $DEFAULT_PATH.'assets/uploads/recipe/images/';
+$DEFAULT_PATH = 'https://infits.in/';
 
 ?>
