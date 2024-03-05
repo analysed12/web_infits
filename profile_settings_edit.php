8fb13b329923882f7f5df66f9963d15b0c85af71
analysed12 <84315274+analysed12@users.noreply.github.com>
2024-03-05 22:40:06 +0530

    updating the profile

--- profile_settings_edit.php
+++ profile_settings_edit.php
@@ -674,9 +674,9 @@ $sql_fetch = "SELECT * FROM dietitian WHERE dietitianuserID = '{$_SESSION['dieti
 
             <div class="content" style="display:flex;gap:1rem;justify-content:center">
                 <a href="whatsapp://send?text=The text to share!" data-action="share/whatsapp/share"><img src="<?= $DEFAULT_PATH ?>assets/images/WhatsApp.svg"></a>
-                <a class="twitter-share-button" href="https://twitter.com/intent/tweet"><img src="<?= $DEFAULT_PATH ?>assets/images/twitter.svg"></a>
-                <a href="https://www.facebook.com/sharer/sharer.php?u=#url" target="_blank"> <img src="<?= $DEFAULT_PATH ?>assets/images/facebook.svg"></a>
-                <a href="https://www.linkedin.com/sharing/share-offsite/?url={url}"><img src="<?= $DEFAULT_PATH ?>assets/images/LinkedIn-Circled.svg"></a>
+                <a class="twitter-share-button" href="https://twitter.com/intent/tweet"><img src="<?= $DEFAULT_PATH ?>assets/images/Twitter.svg"></a>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=#url" target="_blank"> <img src="<?= $DEFAULT_PATH ?>assets/images/Facebook.svg"></a>
+                <a href="https://www.linkedin.com/sharing/share-offsite/?url={url}"><img src="<?= $DEFAULT_PATH ?>assets/images/Linkedin.svg"></a>
                 <img src="<?= $DEFAULT_PATH ?>assets/images/Instagram.svg">
             </div>
         </div>
